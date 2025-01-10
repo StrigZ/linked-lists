e90aa21707f7b3908a9738f641608e3d56a7b2c7
StrigZ <mirgasan200@gmail.com>
2025-01-10 14:04:06 +0300

    Update

--- LinkedList.js
+++ LinkedList.js
@@ -2,121 +2,177 @@ import Node from "./Node.js";
 
 export default class LinkedList {
   constructor(linkedList = {}) {
-    this.list = linkedList;
+    this.headNode = linkedList;
   }
 
   append(value) {
     if (this.isEmpty) {
-      this.list.value = value;
-      this.list.next = null;
+      this.headNode = new Node(value);
       return;
     }
 
-    if (this.list.next) {
-      this.list.next.append(value);
+    if (this.headNode.next !== null) {
+      this.headNode.next.append(value);
     } else {
       const newNode = new LinkedList(new Node(value));
-      this.list.next = newNode;
+      this.headNode.next = newNode;
     }
   }
   prepend(value) {
     if (this.isEmpty) {
-      this.list.value = value;
-      this.list.next = null;
+      this.headNode = new Node(value);
       return;
     }
 
-    const clone = { ...this.list };
+    const clone = { ...this.headNode };
 
-    this.list.value = value;
-    this.list.next = new LinkedList(clone);
+    this.headNode.value = value;
+    this.headNode.next = new LinkedList(clone);
   }
   size() {
-    if (this.list.next === null) {
+    if (this.isEmpty) {
+      return 0;
+    }
+    if (this.headNode.next === null) {
       return 1;
     }
-    return 1 + this.list.next.size();
+    return 1 + this.headNode.next.size();
   }
   head() {
-    return this.list;
+    return this.headNode;
   }
   tail() {
-    if (this.list.next === null) {
-      return this.list;
+    if (this.headNode.next === null) {
+      return this.headNode;
     }
-    return this.list.next.tail();
+    return this.headNode.next.tail();
   }
   at(index) {
-    if (index < 0) {
-      return "Index can't be negative";
-    }
-    if (this.list.next === null) {
-      return "Out of range";
-    }
     if (index === 0) {
-      return this.list;
+      return this.headNode;
     }
-    return this.list.next.at(index - 1);
+    if (index < 0) {
+      throw new Error("Index can't be negative");
+    }
+    if (this.headNode.next === null) {
+      throw new Error("Out of range");
+    }
+    return this.headNode.next.at(index - 1);
   }
   pop() {
-    if (this.list.next.list.next === null) {
-      this.list.next = null;
+    if (this.size() === 0) {
+      throw new Error("List is empty.");
+    }
+    if (this.size() === 1) {
+      this.headNode = {};
       return;
     }
-    return this.list.next.pop();
+    if (this.headNode.next.headNode.next === null) {
+      this.headNode.next = null;
+      return;
+    }
+    return this.headNode.next.pop();
   }
   contains(value) {
-    if (this.list.value === value) {
+    if (this.headNode.value === value) {
       return true;
     }
-    if (this.list.next === null) {
+    if (this.headNode.next === null) {
       return false;
     }
-    return this.list.next.contains(value);
+    return this.headNode.next.contains(value);
   }
-  find(value) {
+  findIndex(value) {
     if (this.contains(value)) {
-      return this._find(value);
+      return this._findIndex(value);
     }
 
     return null;
   }
-  _find(value) {
-    if (this.list.value === value) {
+  _findIndex(value) {
+    if (this.headNode.value === value) {
       return 0;
     }
 
-    return 1 + this.list.next.find(value);
+    return 1 + this.headNode.next.findIndex(value);
+  }
+  find(callback) {
+    /*
+    use callback on current head
+    
+    if callback return true
+      return head
+    else
+      if there is next node
+        pass callback to the next node
+      else 
+      return null
+    */
+    if (callback(this.headNode)) {
+      return this.headNode;
+    }
+    if (this.headNode.next === null) {
+      return null;
+    }
+    return this.headNode.next.find(callback);
   }
   toString() {
-    if (!this.list.next) {
-      return this.list.value;
+    if (this.isEmpty) {
+      return null;
     }
-    return this.list.value + " -> " + this.list.next.toString();
+    if (this.headNode.next === null) {
+      return this.headNode.value;
+    }
+    return this.headNode.value + " -> " + this.headNode.next.toString();
+  }
+  updateAt(value, index) {
+    if (index === 0) {
+      this.headNode.value = value;
+      return;
+    }
+    return this.headNode.next.updateAt(value, index - 1);
   }
   insertAt(value, index) {
     if (index > this.size()) {
-      return "Out of range";
+      throw new Error("Out of range");
     }
     const targetNode = this.at(index);
     const prevToTargetNode = this.at(index - 1);
     const nextNodes = { ...targetNode };
 
     prevToTargetNode.next = new LinkedList(new Node(value));
-    prevToTargetNode.next.list.next = new LinkedList(nextNodes);
+    prevToTargetNode.next.headNode.next = new LinkedList(nextNodes);
   }
   removeAt(index) {
     if (index > this.size()) {
-      return "Out of range";
+      throw new Error("Out of range");
     }
-    const prevToTargetNode = this.at(index - 1);
-    const targetNode = this.at(index);
-    const nextNodes = { ...targetNode.next.list };
+    if (index === 0) {
+      if (this.headNode.next === null) {
+        this.headNode = {};
+      } else {
+        this.headNode = this.headNode.next.headNode;
+      }
+    } else {
+      const prevToTargetNode = this.at(index - 1);
+      const targetNode = this.at(index);
+      if (targetNode.next !== null) {
+        const nextNodes = { ...targetNode.next };
+        prevToTargetNode.next = new LinkedList(nextNodes);
+      } else {
+        this.pop();
+      }
+    }
+  }
 
-    prevToTargetNode.next = new LinkedList(nextNodes);
+  values() {
+    if (this.headNode.next === null) {
+      return [this.headNode.value];
+    }
+    return [this.headNode.value].concat(this.headNode.next.values());
   }
 
   get isEmpty() {
-    return Object.keys(this.list).length === 0;
+    return Object.keys(this.headNode).length === 0;
   }
 }
